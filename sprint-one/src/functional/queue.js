ad7dcbcc9b1634234c97530fd4a9ca32307011f7
Chris Hesterman <chrishesterman14@gmail.com>
2020-02-18 19:31:02 -0800

    Implement code to pass all tests /functional/queue.js
    May refactor next.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,37 @@
 var Queue = function() {
   var someInstance = {};
-
+  var front = 0;
+  var back = 0;
+  var size = 0;
+  var tempIndex;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    back++;
+    size++;
+    someInstance[back] = value;
+    if (size === 1) {
+      front = back;
+    }
   };
 
   someInstance.dequeue = function() {
+    tempIndex = front;
+    storage[tempIndex] = someInstance[front];
+    delete someInstance[front];
+    front++;
+    size--;
+    return storage[tempIndex];
   };
 
   someInstance.size = function() {
+    if (size < 0) {
+      return 0;
+    }
+    return size;
   };
 
   return someInstance;

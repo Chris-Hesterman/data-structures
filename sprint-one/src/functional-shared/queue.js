496f828666f96de9a23c6030ccbda886ac0c8003
Chris Hesterman <chrishesterman14@gmail.com>
2020-02-19 14:10:22 -0800

    Implement basic skeleton for functional-shared/queue.js

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,20 @@
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  let newInstance = {
+    size: 0,
+    front: 0,
+    back: 0
+  };
+  newInstance.enqueue = queueMethods.enqueue;
+  newInstance.dequeue = queueMethods.dequeue;
+  newInstance.size = queueMethods.size;
+
+  return newInstance;
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  enqueue: function(val) {},
+  dequeue: function() {},
+  size: function() {}
+};

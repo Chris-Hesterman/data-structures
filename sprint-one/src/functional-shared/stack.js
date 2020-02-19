adfaccc1b91d6b04a9edd0b1947a31f0c7732ddf
Chris Hesterman <chrishesterman14@gmail.com>
2020-02-19 13:59:37 -0800

    Implement code to pass all tests functional-shared/stack.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,18 +1,34 @@
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
 var Stack = function() {
-  let newInstance = {};
-  let size = 0;
-  let tempStorage = {};
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  let newInstance = {
+    count: 0
+  };
+
+  newInstance.size = stackMethods.size;
   newInstance.push = stackMethods.push;
   newInstance.pop = stackMethods.pop;
-  newInstance.size = stackMethods.size;
 
   return newInstance;
 };
 
 var stackMethods = {
-  push: function() {},
-  pop: function() {},
-  size: function() {}
+  size: function() {
+    return this.count;
+  },
+  push: function(val) {
+    this.count++;
+    this[this.count] = val;
+  },
+  pop: function() {
+    let tempStorage = this[this.count];
+
+    if (this.count <= 0) {
+      return null;
+    }
+    delete this[this.count];
+    this.count--;
+
+    return tempStorage;
+  }
 };

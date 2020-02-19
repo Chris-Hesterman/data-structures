4c9eff3e076d81eb9e438d6ee973bbfe622beb67
Chris Hesterman <chrishesterman14@gmail.com>
2020-02-19 10:55:01 -0800

    Implement basic skeleton for functional-shared/stack.js
    Next will implement methods in stackMethods object;

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,18 @@
 var Stack = function() {
+  let newInstance = {};
+  let size = 0;
+  let tempStorage = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  newInstance.push = stackMethods.push;
+  newInstance.pop = stackMethods.pop;
+  newInstance.size = stackMethods.size;
+
+  return newInstance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function() {},
+  pop: function() {},
+  size: function() {}
+};

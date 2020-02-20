3599233946a02a1ee0276ccd228986ea7c57d1cb
Chris Hesterman <chrishesterman14@gmail.com>
2020-02-20 15:41:00 -0800

    Implement basic skeleton pseudoclassical/stack.js

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,19 @@
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.top = 0;
+  this.storage = {};
 };
 
+let stackMethods = {
+  push: function(value) {},
+  pop: function() {},
+  size: function() {
+    return top;
+  }
+};
 
+Stack.prototype.push = stackMethods.push;
+Stack.prototype.pop = stackMethods.pop;
+Stack.prototype.size = stackMethods.size;

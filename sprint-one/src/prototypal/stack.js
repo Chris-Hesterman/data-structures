12738d2e99af266a4c0439c9b04ad82fe7487d26
Chris Hesterman <chrishesterman14@gmail.com>
2020-02-20 14:32:53 -0800

    Begin implementation of prototypal/stack.js
    Created skeleton and did basic test with size function to
    confirm scoping.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,18 @@
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  let storage = Object.create(Stack.prototype);
+  storage[top] = 0;
 };
 
-var stackMethods = {};
+Stack.prototype.push = stackMethods.push;
+Stack.prototype.pop = stackMethods.pop;
+Stack.prototype.size = stackMethods.size;
 
-
+var stackMethods = {
+  push: function(value) {},
+  pop: function() {},
+  size: function() {
+    return this.top;
+  }
+};

78dc6f9d194d44e8f7dfd43e326e2a91d52c7925
Chris Hesterman <chrishesterman14@gmail.com>
2020-02-20 15:09:51 -0800

    Implement basic skeleton, prototypal/queue.js

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,19 @@
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  let newInstance = Object.create(Queue.prototype);
+  newInstance.front = 0;
+  newInstance.back = 0;
+  newInstance.storage = {};
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  enqueue(value) {},
+  dequeue() {},
+  size() {
+    return back + 1 - front;
+  }
+};
+Queue.prototype.enqueue = queueMethods.enqueue;
+Queue.prototype.dequeue = queueMethods.dequeue;
+Queue.prototype.size = queueMethods.size;

d48a97a398714fed936ef50da5651e0c7d8eee4f
Chris Hesterman <chrishesterman14@gmail.com>
2020-02-20 15:46:57 -0800

    Implement basic skeleton pseudoclassical/queue.js

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,19 @@
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.back = 0;
+  this.front = 0;
+  this.storage = {};
 };
 
+let queueMethods = {
+  enqueue: function(value) {},
+  dequeue: function() {},
+  size: function() {
+    return this.back + 1 - this.front;
+  }
+};
 
+Queue.prototype.enqueue = queueMethods.enqueue;
+Queue.prototype.dequeue = queueMethods.dequeue;
+Queue.prototype.size = queueMethods.size;

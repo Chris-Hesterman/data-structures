fe8570ce86adb696537856b5f225a6e30eb6b91d
Chris Hesterman <chrishesterman14@gmail.com>
2020-02-20 17:12:55 -0800

    Implement basic skeleton for es6/queue.js

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,17 @@
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.back = 0;
+    this.front = 0;
+    this.storage = {};
   }
 
+  enqueue(value) {}
+
+  dequeue() {}
+
+  size() {
+    return this.back + 1 - this.front;
+  }
 }

2f5af86d87af21a6c3e4319088869cf0b9461505
Chris Hesterman <chrishesterman14@gmail.com>
2020-02-20 17:07:34 -0800

    Implement basic skeleton for es6/stack.js

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,16 @@
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.top = 0;
   }
 
+  push(value) {}
+
+  pop() {}
+
+  size() {
+    return this.top;
+  }
 }
